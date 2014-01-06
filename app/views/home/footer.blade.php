bf60bf5660538720fddebeae6c583f5a8118485c
Tom Lagier <lagierta@gmail.com>
2014-01-05 18:59:31 -0800

    Added Google Analytics tracking script

--- app/views/home/footer.blade.php
+++ app/views/home/footer.blade.php
@@ -9,4 +9,14 @@
 <script src="js/main.js"></script>
 <script src="js/load.js"></script>
 <script src="js/navigation.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46861651-1', 'tacticaldesign.la');
+  ga('send', 'pageview');
+
+</script>
 <!-- End footer scripts -->
