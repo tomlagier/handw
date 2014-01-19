909301eca32dddaf04cee71242b6468029c21949
Thomas Lagier <lagierta@gmail.com>
2014-01-18 17:52:06 -0800

    Reverted master.blade.php

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,33 +1,20 @@
 <!DOCTYPE html>
  <html>
-	<head>
- 		@yield('head')
-	</head>
-	 <body>
-<<<<<<< HEAD
-		 <div id="header">
-		 	@yield('header')
-		 </div>
-		 @include('home.side-nav')
-		 <div id="content">
-		 	@yield('content')
-		 </div>
-		 <div id="footer">
-		 	@yield('footer')
-		 </div>
-=======
-	     <div class="window-green-line top"></div>
-			 <div id="header">
-			 	@yield('header')
-			 </div>
-			 @include('home.side-nav')
-			 <div id="content">
-			 	@yield('content')
-			 </div>
-			 <div id="footer">
-			 	@yield('footer')
-			 </div>
-		<div class="window-green-line bottom"></div>
->>>>>>> ac0dbc46bbbccb5ba43767050261728eddba6849
-	 </body>
+        <head>
+                 @yield('head')
+        </head>
+         <body>
+             <div class="window-green-line top"></div>
+                         <div id="header">
+                                 @yield('header')
+                         </div>
+                         @include('home.side-nav')
+                         <div id="content">
+                                 @yield('content')
+                         </div>
+                         <div id="footer">
+                                 @yield('footer')
+                         </div>
+                <div class="window-green-line bottom"></div>
+         </body>
  </html> 
